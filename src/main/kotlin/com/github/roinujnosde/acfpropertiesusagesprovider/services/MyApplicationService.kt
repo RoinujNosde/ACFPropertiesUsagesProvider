efdf91f97f94eb3ca8ef78f6ee2319467216d566
GitHub Action <action@github.com>
2020-09-07 15:30:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/roinujnosde/acfpropertiesusagesprovider/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.roinujnosde.acfpropertiesusagesprovider.services
+
+import com.github.roinujnosde.acfpropertiesusagesprovider.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/roinujnosde/acfpropertiesusagesprovider/services/MyProjectService.kt
+++ src/main/kotlin/com/github/roinujnosde/acfpropertiesusagesprovider/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.roinujnosde.acfpropertiesusagesprovider.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.roinujnosde.acfpropertiesusagesprovider.MyBundle
 
 class MyProjectService(project: Project) {
 
